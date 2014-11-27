22c72dec88ae1ac92c9dbc891795e6fdff64fe73
mkornasz <mantus92@gmail.com>
2014-11-27 23:19:44 +0100

    branch update

---
+++ App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows;
+
+namespace KinectFittingRoom
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        App()
+        {
+            System.Threading.Thread.Sleep(2000);
+        }
+    }
+}
