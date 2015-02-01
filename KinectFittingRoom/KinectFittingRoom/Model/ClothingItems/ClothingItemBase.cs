1143b2a922c69b33a6c6a2594b5e3180d79cbe01
monkog <mkmonikakogut@gmail.com>
2015-02-01 23:42:54 +0100

    Fixed resizing problem

--- KinectFittingRoom/KinectFittingRoom/Model/ClothingItems/ClothingItemBase.cs
+++ KinectFittingRoom/KinectFittingRoom/Model/ClothingItems/ClothingItemBase.cs
@@ -291,7 +291,7 @@ namespace KinectFittingRoom.Model.ClothingItems
         {
             if (joint1.TrackingState == JointTrackingState.NotTracked
                 || joint2.TrackingState == JointTrackingState.NotTracked)
-                WidthScale = _heightScale = 0.1;
+                return;
 
             var joint1Position = KinectService.GetJointPoint(joint1, sensor, width, height);
             var joint2Position = KinectService.GetJointPoint(joint2, sensor, width, height);
